dda11ff1a5d1bf9755296984ba05f9b53f56d12a
Jade <jrenegarcia11@gmail.com>
2024-05-20 23:40:27 -0600

    added templates for other pages

--- flaskapp.py
+++ flaskapp.py
@@ -6,16 +6,23 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
 	return render_template("index.html")
-
+#Music
+@app.route("/music")
+def music():
+	return render_template("music.html")
 #About me
-@app.route("/about")
+@app.route("/about-me")
 def about_me():
 	return render_template("about-me.html")
-
+#Press kit
 @app.route("/press-kit")
 def press_kit():
 	return render_template("press-kit.html")
-
+#Tour
+@app.route("/tour")
+def tour():
+	return render_template("tour.html")
+@app.route
 #404
 @app.errorhandler(404)
 def page_not_found(error):
