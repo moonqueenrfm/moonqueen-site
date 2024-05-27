e41b9e7071fba9ee22ea2f807a0f82934c70ce53
Jade <jrenegarcia11@gmail.com>
2024-05-27 14:43:54 -0600

    Modified grid layout and added stock album cover

--- flaskapp.py
+++ flaskapp.py
@@ -9,21 +9,21 @@ def hello_world():
 #Music
 @app.route("/music")
 def music():
-	return render_template("music.html")
+	return render_template("music.html", title="Music")
 #About me
 @app.route("/about-me")
 def about_me():
-	return render_template("about-me.html")
+	return render_template("about-me.html", title="About")
 #Press kit
 @app.route("/press-kit")
 def press_kit():
-	return render_template("press-kit.html")
+	return render_template("press-kit.html", title="Press Kit")
 #Tour
 @app.route("/tour")
 def tour():
-	return render_template("tour.html")
+	return render_template("tour.html", title="Tour")
 @app.route
 #404
 @app.errorhandler(404)
 def page_not_found(error):
-	return render_template("404.html"), 404
+	return render_template("404.html", title="Not Found"), 404
